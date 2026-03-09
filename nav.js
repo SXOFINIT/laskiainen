ccbb4747c5eec42d1c291f89b5335b3614c0930f
SXOFINIT <136583338+SXOFINIT@users.noreply.github.com>
2026-03-09 13:59:50 +0200

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const nav = `
+  <div class="app-nav">
+      <div class="nav-left">
+          <a href="index.html" class="nav-logo">Laskiainen</a>
+      </div>
+
+      <div class="nav-right">
+          <a href="index.html" class="nav-link">🏠 Etusivu</a>
+          <a href="laskiainenV13.html" class="nav-link">SoLaskiainen</a>
+          <a href="jalkilaskiainen.html" class="nav-link">Jälkilaskiainen</a>
+      </div>
+  </div>
+  `;
+
+  const container = document.createElement("div");
+  container.innerHTML = nav;
+
+  document.body.prepend(container);
+});
